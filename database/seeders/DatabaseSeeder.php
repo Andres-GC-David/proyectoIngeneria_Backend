cf285c55faf63978118b6f57309e2db5ad5006b3
ANDRES.GUTIERREZCORALES@ucr.ac.cr <ANDRES.GUTIERREZCORALES@ucr.ac.cr>
2024-06-17 22:18:02 -0600

    Modificacion de seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,18 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Chofer;
+use App\Models\ChoferViaje;
+use App\Models\ClienteViaje;
+use App\Models\Color;
+use App\Models\EstadoConfirmacion;
+use App\Models\Marca;
+use App\Models\Ruta;
+use App\Models\TipoPago;
+use App\Models\TipoUsuario;
+use App\Models\Usuario;
+use App\Models\Vehiculo;
+use App\Models\Viaje;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +25,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            TipoUsuarioTableSeeder::class,
+            UsuarioTableSeeder::class,
+            ClienteTableSeeder::class,
+            MarcaTableSeeder::class,
+            ColorTableSeeder::class,
+            VehiculoTableSeeder::class,
+            ChoferTableSeeder::class,
+            RutaTableSeeder::class,
+            EstadoConfirmacionTableSeeder::class,
+            ViajeTableSeeder::class,
+            TipoPagoTableSeeder::class,
+            ClienteViajeTableSeeder::class,
+            ChoferViajeTableSeeder::class,
         ]);
     }
 }
